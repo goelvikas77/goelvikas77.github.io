b9abcd2f356a55730b9262982840ce816a323937
goelvikas77 <66691110+goelvikas77@users.noreply.github.com>
2020-07-19 15:46:29 +0530

    Create test.js

---
+++ Connect4/js/test.js
@@ -0,0 +1,10 @@
+var programCode = function(processingInstance) {
+with (processingInstance) {
+    size(600, 600); 
+    frameRate(30);
+	ellipse(300,300,200,200);
+   }};
+  // Get the canvas that ProcessingJS will use
+  var canvas = document.getElementById("mycanvas"); 
+  // Pass the function to ProcessingJS constructor
+  var processingInstance = new Processing(canvas, programCode); 
