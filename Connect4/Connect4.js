f538d5342838a5b1e412e9100233b8be5ab67b3d
goelvikas77 <66691110+goelvikas77@users.noreply.github.com>
2020-07-19 15:30:18 +0530

    Update Connect4.js

--- Connect4/Connect4.js
+++ Connect4/Connect4.js
@@ -2,7 +2,7 @@ var programCode = function(processingInstance) {
   with (processingInstance) {
     size(600, 600); 
     frameRate(30);
-    
+    /*
     // constant variables
     var bottom_banner_height = 70;
     var NUM_COLS = 7;
@@ -342,7 +342,8 @@ var programCode = function(processingInstance) {
             text("Congratulations!!" , 130 , 30);
         }
 
-    };
+    };*/
+    ellipse(100,100,200,200);
 
    }};
   // Get the canvas that ProcessingJS will use
